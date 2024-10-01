138e4e70555f9c40920e46d2e668691b9ae593b1
SigningOwl <whddms1208@gmail.com>
2024-10-01 17:34:08 +0900

    Modify github actions workflow to cache node_modules and vitest.config.js to use its own config file.

--- vitest.config.js
+++ vitest.config.js
@@ -1,19 +1,27 @@
 import { defineConfig } from 'vitest/config';
-import { mergeConfig } from 'vite';
-import viteconfig from './vite.config';
 import { JSDOM } from 'jsdom';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import dotenv from 'dotenv';
 
-// 기존 vite 설정을 가져와 테스트 설정과 병합
-export default mergeConfig(
-  viteconfig || {},
-  defineConfig({
-    test: {
-      environment: 'jsdom', // browser simulation
-      coverage: {
-        provider: 'v8',
-        reporter: ['text', 'lcov'],
-        reportsDirectory: './coverage',
-      },
+dotenv.config();
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './coverage',
     },
-  })
-);
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  define: {
+    'process.env': process.env,
+  },
+});
