a6bfd0a87a77d057c0789f1833e232d96dc4d80c
SigningOwl <whddms1208@gmail.com>
2024-10-01 17:22:14 +0900

    Modify vitest.config.js to ensure merging vite config

--- vitest.config.js
+++ vitest.config.js
@@ -3,8 +3,9 @@ import { mergeConfig } from 'vite';
 import viteconfig from './vite.config';
 import { JSDOM } from 'jsdom';
 
+// 기존 vite 설정을 가져와 테스트 설정과 병합
 export default mergeConfig(
-  viteconfig,
+  viteconfig || {},
   defineConfig({
     test: {
       environment: 'jsdom', // browser simulation
